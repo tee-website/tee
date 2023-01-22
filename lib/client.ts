f30c02d78aa1c3a3c5820d10da1bc9af3ca08eb1
joelxhenry <joel.henry.023@gmail.com>
2023-01-22 05:47:54 -0500

    Deployment Update

--- lib/client.ts
+++ lib/client.ts
@@ -3,11 +3,10 @@ import builder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SAN_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SAN_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2023-01-12",
   useCdn: true,
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
 
 export const urlFor = (source: SanityImageSource) =>
